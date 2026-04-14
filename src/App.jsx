84c88a23bff5e415e572ae56df8a30bdfeee3816
Mayur <mayur240624@gmail.com>
2026-04-14 16:05:22 +0530

    Added dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import Vision from './components/Vision';
+<<<<<<< Updated upstream
 import CTA from './components/CTA';
 import Footer from './components/Footer';
 import Signup from "./pages/Signup.jsx";
@@ -30,6 +31,22 @@ function App() {
           <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
+=======
+import CTA from './components/CTA';     // Import the banner
+import Footer from './components/Footer'; // Import the footer
+import Equipment_Feed_and_Home from './Pages/Equipment_Feed_and_Home.jsx';
+
+function App() {
+  return (
+    <div className="w-full min-h-screen bg-[#F1F4F2] font-sans antialiased overflow-x-hidden">
+      <Hero/>
+      {/* <Equipment_Feed_and_Home /> */}
+      <Features />
+      <Vision />
+      <CTA />     Add it here
+      <Footer />  {/* Add it here */}
+    </div>
+>>>>>>> Stashed changes
   );
 }
 
