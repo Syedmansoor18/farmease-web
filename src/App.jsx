7d8cf0fba4f0104aab2752690daf5e0a3271dda2
Mayur <mayur240624@gmail.com>
2026-04-14 16:33:48 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import Vision from './components/Vision';
-<<<<<<< Updated upstream
 import CTA from './components/CTA';
 import Footer from './components/Footer';
 import Signup from "./pages/Signup.jsx";
 import Login from "./pages/Login.jsx";
+import Equipment_Feed_and_Home from './Dashboard/Equipment_Feed_and_Home.jsx';
+
 
 // This component represents your full Landing Page
 const LandingPage = () => (
@@ -29,24 +30,9 @@ function App() {
         {/* Route for the Signup Page */}
         <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/equipments" element={<Equipment_Feed_and_Home />} />
       </Routes>
     </Router>
-=======
-import CTA from './components/CTA';     // Import the banner
-import Footer from './components/Footer'; // Import the footer
-import Equipment_Feed_and_Home from './Pages/Equipment_Feed_and_Home.jsx';
-
-function App() {
-  return (
-    <div className="w-full min-h-screen bg-[#F1F4F2] font-sans antialiased overflow-x-hidden">
-      <Hero/>
-      {/* <Equipment_Feed_and_Home /> */}
-      <Features />
-      <Vision />
-      <CTA />     Add it here
-      <Footer />  {/* Add it here */}
-    </div>
->>>>>>> Stashed changes
   );
 }
 
